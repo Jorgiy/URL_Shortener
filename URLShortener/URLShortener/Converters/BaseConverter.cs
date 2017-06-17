fa915bbd35e10c83644cfd5c01babdbeb32ab8f5
Uriy <jorgiy@bk.ru>
2017-06-17 17:19:24 +0300

    fix

---
+++ URLShortener/URLShortener/Converters/BaseConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace URLShortener.Converters
+{
+    /// <summary>
+    /// Для конвертирования из псевдослучайных чисел в строку
+    /// </summary>
+    public static class BaseConverter
+    {
+        /// <summary>
+        /// Преобразовывает число в короткую строку base36
+        /// </summary>
+        /// <param name="input">число для преобразования</param>
+        /// <returns>результат конвертации</returns>
+        public static string ConvertFrom10To36(uint input)
+        {
+            return Convert.ToString(input, 36);
+        }
+    }
+}
--- URLShortener/URLShortener/Converters/CongruentMethod.cs
+++ URLShortener/URLShortener/Converters/CongruentMethod.cs
@@ -32,20 +32,4 @@ namespace URLShortener.Converters
             return (input * CoefficientA + CoefficientC) % CoefficientM;
         }
     }
-
-    /// <summary>
-    /// Для конвертирования из псевдослучайных чисел в строку
-    /// </summary>
-    public static class BaseConverter
-    {
-        /// <summary>
-        /// Преобразовывает число в короткую строку base36
-        /// </summary>
-        /// <param name="input">число для преобразования</param>
-        /// <returns>результат конвертации</returns>
-        public static string ConvertFrom10To36(uint input)
-        {
-            return Convert.ToString(input, 36);
-        }
-    }
 }
