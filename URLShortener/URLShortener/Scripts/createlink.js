dffa44afc5d73f11e2cedfc8225e52822dee8b85
Uriy <jorgiy@bk.ru>
2017-06-20 16:07:34 +0300

    + knockout beginnigs

--- URLShortener/URLShortener/Scripts/createlink.js
+++ URLShortener/URLShortener/Scripts/createlink.js
@@ -1,1 +1,20 @@
-﻿
+﻿$(document).ready(InitPage);
+
+function InitPage() {
+    var viewModel = {
+        shortLink: function() {
+            var input = $("#text-box-input").val();
+            alert(input);
+        },
+
+        returnToMainLink: function() {
+            
+        },
+
+        copyTextToBuffer : function() {
+            
+        }
+    }
+
+    ko.applyBindings(viewModel);
+}
